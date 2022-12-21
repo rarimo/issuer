d53f7bd6404ad21eb002d1722206ef4efaa64e2f
Levochko Anton <hykakdela@gmail.com>
2022-12-21 15:16:09 +0200

    removed cop

--- internal/config/main.go
+++ internal/config/main.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"gitlab.com/distributed_lab/kit/comfig"
-	"gitlab.com/distributed_lab/kit/copus"
-	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/kit/kv"
 	"gitlab.com/distributed_lab/kit/pgdb"
 )
@@ -11,14 +9,12 @@ import (
 type Config interface {
 	comfig.Logger
 	pgdb.Databaser
-	types.Copuser
 	comfig.Listenerer
 }
 
 type config struct {
 	comfig.Logger
 	pgdb.Databaser
-	types.Copuser
 	comfig.Listenerer
 	getter kv.Getter
 }
@@ -27,7 +23,6 @@ func New(getter kv.Getter) Config {
 	return &config{
 		getter:     getter,
 		Databaser:  pgdb.NewDatabaser(getter),
-		Copuser:    copus.NewCopuser(getter),
 		Listenerer: comfig.NewListenerer(getter),
 		Logger:     comfig.NewLogger(getter, comfig.LoggerOpts{}),
 	}
--- internal/service/main.go
+++ internal/service/main.go
@@ -4,15 +4,12 @@ import (
 	"net"
 	"net/http"
 
-	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/logan/v3"
-	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/q-dev/q-id/qid-issuer/internal/config"
 )
 
 type service struct {
 	log      *logan.Entry
-	copus    types.Copus
 	listener net.Listener
 }
 
@@ -20,17 +17,12 @@ func (s *service) run() error {
 	s.log.Info("Service started")
 	r := s.router()
 
-	if err := s.copus.RegisterChi(r); err != nil {
-		return errors.Wrap(err, "cop failed")
-	}
-
 	return http.Serve(s.listener, r)
 }
 
 func newService(cfg config.Config) *service {
 	return &service{
 		log:      cfg.Log(),
-		copus:    cfg.Copus(),
 		listener: cfg.Listener(),
 	}
 }
