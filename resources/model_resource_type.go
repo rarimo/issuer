09e8c7aa4a5294f48155cef7a196d7bfd7da3d4c
Anton Levochko <hykakdela@gmail.com>
2023-06-28 15:40:36 +0300

    fix docs

--- resources/model_resource_type.go
+++ resources/model_resource_type.go
@@ -8,9 +8,8 @@ type ResourceType string
 
 // List of ResourceType
 const (
-	CLAIM_OFFER   ResourceType = "claim_offer"
-	IDENTIFIER    ResourceType = "identifier"
-	CLAIM_DATA    ResourceType = "claim_data"
-	CLAIM_ID      ResourceType = "claim_id"
-	PUBLISH_STATE ResourceType = "publish_state"
+	CLAIM_OFFER ResourceType = "claim_offer"
+	IDENTIFIER  ResourceType = "identifier"
+	CLAIM_ISSUE ResourceType = "claim_issue"
+	CLAIM_ID    ResourceType = "claim_id"
 )
